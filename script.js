3c72c4cb9784e99e1d76a0c7cf10369a5fa24f01
Murilo <murilo.batista.barros@gmail.com>
2020-06-27 00:16:32 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,32 @@
+var primeiroNumero
+var segundoNumero
+
+var resposta
+
+function numero() {
+    var aleatorio = Math.random()
+    var maximo = aleatorio * 8
+    var exatas = Math.floor(maximo)
+    var resultado = exatas + 2
+    return resultado
+}
+
+function novoJogo() {
+    primeiroNumero = numero()
+    segundoNumero = numero()
+
+    var divResultado = document.getElementById("numA")
+
+    divResultado.innerHTML = primeiroNumero + " x " + segundoNumero
+}
+
+function gerarResultado() {
+    resposta = Number(primeiroNumero * segundoNumero)
+    var inputNumB = document.getElementById("numB")
+    numB = Number(inputNumB.value)
+    if (numB === resposta) {
+        alert('acertou')
+    } else {
+        alert('errou')
+    }
+}
